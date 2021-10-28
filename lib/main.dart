03a2867cd924ccf938513240bbe722c51bb1730b
Joshua Komaba <joshuakomaba@gmail.com>
2021-10-27 20:12:47 -0600

    Corrección de ruta inicial

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'home',
+      initialRoute: 'login',
       routes: {
         'login': ( _ ) => const LoginScreen(),
         'home': ( _ ) => const HomeScreen(),
